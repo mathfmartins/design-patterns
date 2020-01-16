05d8cc8a6c31744ebc4b53f29d5896b56a6f3925
Matheus Martins <math.fmartins42@gmail.com>
2020-01-16 16:46:08 -0300

    Novo projeto para novos patterns

---
+++ outros-patterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace outros_patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
